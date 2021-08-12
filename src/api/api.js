ab27ff97e4d1cc6ad794b9412199f74248a20f79
rabbit <1500679598@qq.com>
2021-08-12 17:54:16 +0800

    修改了接口

--- src/api/api.js
+++ src/api/api.js
@@ -23,33 +23,139 @@ axios.interceptors.response.use(function(response) {
 
 //接口定义的方式；
 export default {
-    //登录
-    // signin: (params) => {
-    //     return axios.post('/user/login', params, postConfig)
-    // },
 
+
+    //题的开始
+    /**
+     * @description  增加题
+     * @params {type[String],title[String],options[Array],result[String],categoryId[Number]}  options:[{key:'A',value:'A的答案'}]
+     * */
     createTopic: (params) => {
         return axios.post('/topic/create', params, postConfig)
     },
-    getCategoryList: (params) => {
-        return axios.post('/category/list', params, postConfig)
-    },
-    createCategory: (params) => {
-        return axios.post('/category/create', params, postConfig)
-    },
-    deleteCategory: (params) => {
-        return axios.post('/category/delete', params, postConfig)
-    },
+
+    /**
+     * @description  获取题列表
+     * @params {id,categoryId,pageNum,pageSize} 可选 id:题id  categoryId:题目类型  pageNum:第几页数据  pageSize:每页条数
+     * */
     getTopicList: (params) => {
         return axios.post('/topic/list', params, postConfig)
     },
-    getArticleDetail: (params) => {
-        return axios.post('/article/detail', params, postConfig)
+
+    /**
+     * @description  编辑题
+     * @params {id,type,title,options,result,categoryId}
+     * */
+    updateTopic: (params) => {
+        return axios.post('/topic/update', params, postConfig)
     },
+
+    /**
+     * @description  删除题
+     * @params {id}
+     * */
+    deleteTopic: (params) => {
+        return axios.post('/topic/delete', params, postConfig)
+    },
+    //题的结束
+
+
+
+    //类目的开始
+    /**
+     * @description  获取类目列表
+     * @params {type[String]} type: 类目类型 1：试题 2：文章；
+     * */
+    getCategoryList: (params) => {
+        return axios.post('/category/list', params, postConfig)
+    },
+
+    /**
+     * @description  增加类目
+     * @params {type[String],key} type: 类目类型 1：试题 2：文章； key:类目文本
+     * */
+    createCategory: (params) => {
+        return axios.post('/category/create', params, postConfig)
+    },
+
+    /**
+     * @description  删除类目
+     * @params {type[String],id} type: 类目类型 1：试题 2：文章； key:类目标识
+     * */
+    deleteCategory: (params) => {
+        return axios.post('/category/delete', params, postConfig)
+    },
+
+    /**
+     * @description  更新类目接口
+     * @params {type[String],id[String],key[String],iconUrl[String]}
+     * */
     updateCategory: (params) => {
         return axios.post('/category/update', params, postConfig)
     },
-    userLogin(params){
+    //类目的结束
+
+
+
+    //文章的开始
+    /**
+     * @description  获取文章详情
+     * @params {id}  id:文章ID
+     * */
+    getArticleDetail: (params) => {
+        return axios.post('/article/detail', params, postConfig)
+    },
+
+    /**
+     * @description  增加文章
+     * @params {title[String],article:[String],categoryId:[String]}
+     * */
+    createArticle: (params) => {
+        return axios.post('/article/create', params, postConfig)
+    },
+
+    /**
+     * @description  获取文章列表
+     * @params {categoryId:[String],pageNum:[String],pageSize:[String]}  categoryId:非必填 文章类型  pageNum:非必填 第几页  pageSize:非必填 获取几条数据 默认为10
+     * */
+    getArticleList: (params) => {
+        return axios.post('/article/list', params, postConfig)
+    },
+    //文章的结束
+
+
+
+    //用户的接口开始
+    /**
+     * @description  用户登录接口
+     * @params {username:[String],password:[String]} 
+     * */
+    userLogin(params) {
         return axios.post('/user/login', params, postConfig)
-    }
+    },
+
+    /**
+     * @description  用户注册接口
+     * @params {username:[String],password:[String]} 
+     * */
+    userRegister(params) {
+        return axios.post('/user/register', params, postConfig)
+    },
+
+    /**
+     * @description  用户信息接口
+     * @params 没有参数 
+     * */
+    getUserInfo(params) {
+        return axios.post('/user/info', params, postConfig)
+    },
+
+    /**
+     * @description  修改用户信息接口
+     * @params {avatorId,avatorName,phoneNumber,mail,password}
+     * */
+    updateUser(params) {
+        return axios.post('/user/update', params, postConfig)
+    },
+    //用户接口结束
 }
--- src/store/index.js
+++ src/store/index.js
@@ -4,14 +4,22 @@ export default createStore({
     state: {},
     mutations: {},
     actions: {
-        /**
-         * @description 用户登录
-         * @param username  用户名
-         * @param password 密码
-         */
+        //题
         createTopic(ctx, payload) {
             return Api.createTopic(payload)
         },
+        getTopicList(ctx, payload) {
+            return Api.getTopicList(payload)
+        },
+        updateTopic(ctx, payload) {
+            return Api.updateTopic(payload)
+        },
+        deleteTopic(ctx, payload) {
+            return Api.deleteTopic(payload)
+        },
+
+
+        //类目
         getCategoryList(ctx, payload) {
             return Api.getCategoryList(payload)
         },
@@ -21,18 +29,35 @@ export default createStore({
         deleteCategory(ctx, payload) {
             return Api.deleteCategory(payload)
         },
-        getTopicList(ctx, payload) {
-            return Api.getTopicList(payload)
-        },
-        getArticleDetail(ctx, payload) {
-            return Api.getArticleDetail(payload)
-        },
         updateCategory(ctx, payload) {
-            return Api.updateCategory(payload)
+            return Api.updateCategory(payload);
         },
-        userLogin(ctx,payload){
-            return Api.userLogin(payload);
-        }
+
+        //文章
+        getArticleDetail(ctx, payload) {
+            return Api.getArticleDetail(payload);
+        },
+        createArticle(ctx, payload) {
+            return Api.createArticle(payload);
+        },
+        getArticleList(ctx, payload) {
+            return Api.getArticleList(payload);
+        },
+
+        //用户
+        userLogin(ctx, payload) {
+            return Api.getArticleList(payload);
+        },
+        userRegister(ctx, payload) {
+            return Api.getArticleList(payload);
+        },
+        getUserInfo(ctx, payload) {
+            return Api.getArticleList(payload);
+        },
+        updateUser(ctx, payload) {
+            return Api.getArticleList(payload);
+        },
+
     },
     modules: {}
 })
