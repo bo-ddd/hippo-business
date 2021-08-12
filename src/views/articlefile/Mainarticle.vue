737474d4e9d6dd6614c5cc90922fb8cadab70603
woneinie <1641400496@qq.com>
2021-08-12 10:48:35 +0800

    页面更新

--- src/views/articlefile/Mainarticle.vue
+++ src/views/articlefile/Mainarticle.vue
@@ -50,9 +50,9 @@ export default {
   height: 50px;
   bottom: 0;
   display: flex;
-  justify-content: start;
+  justify-content: flex-start;
   align-items: center;
-}
+} 
 .item {
   margin-left: 100px;
 }
