28c1bdf344fa95e5dd02a0d4d4bb81594f49d248
ljl <1773182155@qq.com>
2021-08-12 08:26:45 +0800

    ljl

---
+++ src/store/index.js
@@ -0,0 +1,33 @@
+import { createStore } from 'vuex'
+import Api from '@/api/api'
+export default createStore({
+    state: {},
+    mutations: {},
+    actions: {
+        /**
+         * @description 用户登录
+         * @param username  用户名
+         * @param password 密码
+         */
+        create(ctx, payload) {
+            return Api.create(payload)
+        },
+        catlist(ctx, payload) {
+            return Api.catlist(payload)
+        },
+        catcreate(ctx, payload) {
+            return Api.catcreate(payload)
+        },
+        catdelete(ctx, payload) {
+            return Api.catdelete(payload)
+        },
+        topiclist(ctx, payload) {
+            return Api.topiclist(payload)
+        },
+        detail(ctx, payload) {
+            return Api.detail(payload)
+        },
+    },
+    modules: {}
+})
+console.log(Api);
