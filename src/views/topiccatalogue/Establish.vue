14aee4fbc65deb8bd0a9ca3159addf7ab0cc029c
rabbit <1500679598@qq.com>
2021-08-12 14:43:41 +0800

    新加问题详情页

--- src/views/topiccatalogue/Establish.vue
+++ src/views/topiccatalogue/Establish.vue
@@ -117,6 +117,14 @@ export default {
                 console.log('选择题答案不得低于两个')
             } else if (this.data.type == 2 && (this.data.result.split(',').length < 2)) {
                 console.log('多选题答案不得小于两个')
+            }else if(this.data.title==''){
+                console.log('请输入题目')
+            }else if(this.data.result==''){
+                console.log('请选择答案')
+            }else if(this.data.categoryId==0){
+                console.log('请选择题目类型')
+            }else if(this.data.type==''){
+                console.log('请选择该题类型')
             } else {
                 if (this.single.length != 0) {
                     this.data.result = this.option[this.single]
